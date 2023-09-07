b8c39f036cd0565e08ab530a65e3cfde26f8d9ca
qkdwngus <arkhyeon@gmail.com>
2023-09-07 09:57:49 +0900

    [feat] build 용량 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: id => {
+          if (id.includes('node_modules')) {
+            const module = id.split('node_modules/').pop().split('/')[0];
+            return `vendor/${module}`;
+          }
+        },
+      },
+    },
+  },
 });
