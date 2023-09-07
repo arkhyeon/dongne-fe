b6f3cfd63472e8c2cdb612113c015f4613321313
qkdwngus <arkhyeon@gmail.com>
2023-09-07 16:49:38 +0900

    [test] Lazy Loading 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './page';
-import Main from './page/main/Main';
 import BoardMain from './page/main/BoardMain';
 import BoardList from './page/boardList/BoardList';
 import PostDetail from './page/postDetail/PostDetail';
@@ -15,13 +14,38 @@ import MemberEdit from './page/member/components/MemberEdit';
 import EventBoardList from './page/boardList/EventBoardList';
 import MemberRanking from './page/member/MemberRanking';
 import { HasLogin, PrivateRouter } from './component/router/RouterBranch';
+import { ComponentType, lazy, LazyExoticComponent } from 'react';
+import Main from './page/main/Main';
+
+// const [Main, preload] = getLazyComponentAndPreload(() => import('./page/main/Main'));
+// const [Main, preload] = getLazyComponentAndPreload(() => import('./page/main/Main'));
+// const [Main, preload] = getLazyComponentAndPreload('./page/main/Main');
+// const Main = lazy(() => MainPromise);
+// const MainPromise = import('./page/main/Main');
+// const Main = lazy(() => import('./page/main/Main'));
 
 function App() {
+  // useEffect(() => {
+  //   preload();
+  // }, []);
+  // const di = () => {
+  //   import('./common/test').then(test => {
+  //     console.log(test.text());
+  //   });
+  // };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />}>
-          <Route path="/" element={<Main />}>
+          <Route
+            path="/"
+            element={
+              //<Suspense fallback={<div>...Loading</div>}>
+              <Main />
+              //</Suspense>
+            }
+          >
             <Route index path="/" element={<BoardMain />} />
             <Route path="board" element={<BoardList />} />
             <Route path="eventboard" element={<EventBoardList />} />
@@ -44,3 +68,22 @@ function App() {
 }
 
 export default App;
+export type ReactLazyFactory<T = any> = () => Promise<{ default: ComponentType<T> }>;
+
+export type ComponentPreloadTuple<T = any> = [
+  component: LazyExoticComponent<ComponentType<T>>,
+  preloadFn: () => void,
+];
+
+export function getLazyComponentAndPreload<T = any>(
+  componentPath: string,
+): ComponentPreloadTuple<T>;
+export function getLazyComponentAndPreload<T = any>(
+  factory: ReactLazyFactory<T>,
+): ComponentPreloadTuple<T>;
+export function getLazyComponentAndPreload<T = any>(
+  input: string | ReactLazyFactory<T>,
+): ComponentPreloadTuple<T> {
+  const factory = () => (typeof input === 'string' ? import(input) : input());
+  return [lazy(factory), factory];
+}
