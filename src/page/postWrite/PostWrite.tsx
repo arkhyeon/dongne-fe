b42601261bb5753f4a40b78f07ea501539ff093e
qkdwngus <arkhyeon@gmail.com>
2024-03-12 13:17:04 +0900

    [feat] Build Error 수정 및 빌드 완료

--- src/page/postWrite/PostWrite.tsx
+++ src/page/postWrite/PostWrite.tsx
@@ -57,8 +57,8 @@ function PostWrite() {
       channelName,
       subCategory,
       mainCategory,
-      cityCode: getCookie('cityCode'),
-      zoneCode: getCookie('zoneCode'),
+      cityCode: getCookie('cityCode') || '',
+      zoneCode: getCookie('zoneCode') || '',
       deadLineAt: boardType === 'NORMAL' ? null : deadLineAt,
       fileImg,
     };
